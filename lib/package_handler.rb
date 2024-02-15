82801d730a5913d6c2c8d193257403f852e0bce8
jgillman <jronaldgillman@gmail.com>
2024-02-14 18:59:05 -0600

    package_handler module now determines OS/Distro and Package manager

--- lib/package_handler.rb
+++ lib/package_handler.rb
@@ -1,19 +1,99 @@
 #
-# Handles installation and removal of packages.
+# Handles installation and removal of Applications using a package manager.
 # TODO: Add support for other package managers and maybe compilation...?
 #
 
 module Package_handler
     def self.install_software(name)
-        system("dnf install -y #{name}")
-        test_software(name)
+       cmd = make_command('i') + "-y #{name}"
+       system(cmd)
+       test_software(name)
     end
 
-    def self.remove_software(name)
-        system("dnf remove -y #{name}")
+    def self.remove_software(name) 
+        cmd = make_command('r') + "-y #{name}"
+        system(cmd)
     end
 
     def self.test_software(name)
         system(name)
+    end
+
+    def self.make_command(arg)
+        if get_operating_system == 'linux'
+            return make_linux_command(arg)
+        end
+        if get_operating_system == 'mac'
+            if arg == 'i'
+                return 'brew install '
+            else
+                return 'brew remove '
+            end
+        end
+        if get_operating_system == 'openbsd'
+            if arg == 'i'
+                return 'pkg_add '
+            else
+                return 'pkg_delete '
+            end
+        end
+    end
+
+    def self.make_linux_command(arg)
+        package_manager = get_linux_package_manager
+
+        if package_manager == 'dnf'
+            if arg == 'i'
+                return 'dnf install '
+            else
+                return 'dnf remove '
+            end
+        elsif package_manager == 'apt'
+            if arg == 'i'
+                return 'apt-get install '
+            else
+                return 'apt-get remove '
+            end
+        elsif package_manager == 'pacman'
+            if arg == 'i'
+                return 'pacman -S '
+            else
+                return 'pacman -R '
+            end
+        elsif package_manager == 'emerge'
+            if arg == 'i'
+                return 'emerge '
+            else
+                return 'emerge -C '
+            end
+        end
+    end
+
+    def self.get_operating_system
+        if RUBY_PLATFORM.include? "linux"
+            return 'linux'
+        elsif RUBY_PLATFORM.include? "darwin"
+            return 'mac'
+        elsif RUBY_PLATFORM.include? "openbsd"
+            return 'openbsd'
+        else
+            puts "ERROR: I don't know how to handle this OS: #{RUBY_PLATFORM}"
+            exit 1
+        end
+    end
+
+    def self.get_linux_package_manager
+        if File.exist?('/etc/fedora-release')
+            return 'dnf'
+        elsif File.exist?('/etc/debian_version')
+            return 'apt'
+        elsif File.exist?('/etc/arch-release')
+            return 'pacman'
+        elsif File.exist?('/etc/gentoo-release')
+            return 'emerge'
+        else
+            puts "ERROR: Can't determine package manager!:"
+            exit 1
+        end  
     end
 end
--- lib/shopping_list.rb
+++ lib/shopping_list.rb
@@ -5,15 +5,30 @@ module Shopping_list
   Version = '0.0.1'
 
   def self.yaml_to_hash(file)
-    hash = YAML.load_file(file)
-    hash['install_software'].each { |key| Package_handler.install_software(key.delete('"')) }
-    hash['remove_software'].each { |key| Package_handler.remove_software(key.delete('"')) }
-    puts hash['config'].inspect
+    list = YAML.load_file(file)
+
+    software_check(list)
+
+    
+    puts list['config'].inspect
+  end
+
+
+  def self.software_check(list)
+    if list['install_software'].nil?
+      puts "No software to install!"
+    else
+      list['install_software'].each { |key| Package_handler.install_software(key.delete('"')) }
+    end
+    if list['remove_software'].nil?
+      puts "No software to remove!"
+    else
+      list['remove_software'].each { |key| Package_handler.remove_software(key.delete('"')) }
+    end
   end
 
   def self.output_file(file)
     puts "Outputting file: #{file}"
-
     yaml_to_hash(file)
   end
 end
